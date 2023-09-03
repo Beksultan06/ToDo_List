8980611722ff70bc1c014018e4fcd5f1b791dc9a
didiotghima <nurlanuuulubeksultan@gmail.com>
2023-09-03 17:28:46 -0500

    пятый коммит

--- apps/todo/views.py
+++ apps/todo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from apps.todo.models import Task
+from apps.todo.serializers import TaskSerializer
 # Create your views here.
+class TodoAPI():
+    queruset = Task.objects.all()
+    serialaizers_class = TaskSerializer
+
+    
