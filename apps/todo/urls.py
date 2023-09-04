2158b1657352af505cf2a39eb8a1b0f3a053f840
didiotghima <nurlanuuulubeksultan@gmail.com>
2023-09-04 09:31:49 -0500

    add token

--- apps/todo/urls.py
+++ apps/todo/urls.py
@@ -2,7 +2,14 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import TaskAPIViewSet
 
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
 router = DefaultRouter()
 router.register('tasks/', TaskAPIViewSet, basename='task')
+
+urlspatterns = [
+    path("task", TokenObtainPairView.as_view(), name='task'),
+]
 
 urlpatterns = router.urls
