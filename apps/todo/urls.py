dfd2e2ae19fc57f863248bd86cfec1b05ec83ccd
didiotghima <nurlanuuulubeksultan@gmail.com>
2023-09-04 09:28:29 -0500

    add admin

--- apps/todo/urls.py
+++ apps/todo/urls.py
@@ -3,6 +3,6 @@ from rest_framework.routers import DefaultRouter
 from .views import TaskAPIViewSet
 
 router = DefaultRouter()
-router.register(r'tasks', TaskAPIViewSet, basename='task')
+router.register('tasks/', TaskAPIViewSet, basename='task')
 
 urlpatterns = router.urls
